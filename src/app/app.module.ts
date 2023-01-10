856005d5c39c5041baa383480936734ab18d1c93
Mayra TS <mayra.be7686@gmail.com>
2023-01-10 19:53:05 -0300

    Template sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  templateUrl: './portafolio.component.html',
+  styleUrls: ['./portafolio.component.css']
+})
+export class PortafolioComponent {
+
+}
