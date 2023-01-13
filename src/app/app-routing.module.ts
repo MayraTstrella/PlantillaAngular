1167d0d4c059f98ffa775e86685b462381e8205e
Mayra TS <mayra.be7686@gmail.com>
2023-01-12 21:16:56 -0300

    config de links

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from "./pages/about/about.component";
+import { ItemComponent } from "./pages/item/item.component";
+import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
+
+const app_routes: Routes = [
+     { path: 'home', component: PortafolioComponent },
+     { path: 'about', component: AboutComponent },
+     { path: 'item', component: ItemComponent },
+     { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    
+
+]
+
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(app_routes, {useHash: true})
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRouting { 
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AppRouting } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
